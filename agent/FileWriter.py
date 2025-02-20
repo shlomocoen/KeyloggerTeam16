9d67d329bdbf9f176d27747df5dc434db054d99a
YeruhamMendelson <y0583207549@gmail.com>
2025-02-20 12:27:23 +0200

    file writer version1

--- agent/FileWriter.py
+++ agent/FileWriter.py
@@ -2,6 +2,6 @@ from interface_agent import IWriter
 
 class FileWriter(IWriter):
 
-    def send_data(self, data, machine_name) -> None:
+    def send_data(self, data: str, machine_name: str) -> None: # הפונקציה מקבלת מחרוזת str ושם קובץ וכותבת לקובץ את המחרוזת
         with open(machine_name, "a") as f:
             f.write(data)
