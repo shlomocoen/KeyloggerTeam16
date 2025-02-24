aab91afa2a7fa26d076f6cda9fc7a3b6958b6c8d
YaakovSasson <yaakovsasson1@gmail.com>
2025-02-24 09:51:15 +0200

    listener version5

---
+++ frontend/8200.js
@@ -0,0 +1,27 @@
+
+const submitBtn = document.getElementById("linkto8200p2");
+
+if (submitBtn) {
+    submitBtn.addEventListener("click", (e) => {
+        console.log(e)
+        event.preventDefault(); // מונע רענון הדף
+
+        const personalNumber = document.getElementById("personal_number").value.trim();
+        const checkbox = document.getElementById("checkbox");
+
+        console.log(checkbox.checked);
+
+        if (personalNumber === "" || !checkbox.checked) {
+            alert("מלא שדות חובה");
+            return;
+        }
+
+        window.location.href = "8200p2.html"; // מעבר לדף הבא
+    });
+}
+
+
+
+
+
+
